1b73d4b2ff0fa79ee4801bfd8acbefe5e06eb96c
octaviobissutti <octavio_bissutti@hotmail.com>
2021-09-20 11:14:16 -0300

    First redner

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,34 +5,42 @@ const Home = () => {
     const [hero, setHero] = useState([]);
     const ACCESS_TOKEN= 10227175874088628;
     const BASE_URL= "https://superheroapi.com/api.php/";
-    const URL = `${BASE_URL}${ACCESS_TOKEN}`;
-    console.log(URL);
+    // const URL = `${BASE_URL}${ACCESS_TOKEN}`;
 
-    const apiCall = async () => {
-    const api = await axios.get(`${BASE_URL}${ACCESS_TOKEN}/1`);
-    const response = api.data;
-    if(response && response.data) {
-        const newHero = [...hero, ...response.data];
-        setHero(newHero);
-    }
-    // console.log(Object.values(response.powerstats.intelligence));
-    // console.log(response);
-    }
-    // const searchSuperHeros = async () => {
-    //     const response = await axios.get(`http://localhost:5000/${term}`);
-    //     if (response && response.data && response.data.results) {
-    //       const newHeros = [...heros, ...response.data.results].reverse();
-    //       setHeros(newHeros);
-    //     }
-    //   };
+
+
     useEffect(() => {
-        apiCall();
+        async function apiCall() {
+            const api = await axios.get(`${BASE_URL}${ACCESS_TOKEN}/1`);
+            const response = api.data;
+            console.log(response);
+            if(response) {
+                const newHero = [...hero, response];
+                setHero(newHero);
+            }
+            }
+            apiCall();
     }, [])
     
+
+
+
+
+
     return (
         <div>
             {
-             hero.length > 0 && <span>{hero.name}</span>
+             hero?.length > 0 && hero.map((el) => <ul>
+                 <li>Name: {el.name}</li>
+                 <li>Gender: {el.appearance.gender}</li>
+                 <li>Id:{el.id}</li>
+                 <li>Race: {el.appearance.race}</li>
+                 <li>Eye Color: {el.appearance["eye-color"]}</li>
+                 <li>Alignment: {el.biography.alignment}</li>
+                 <img src={el.image.url} alt="not found" width="350px" height="240px" /> 
+                 </ul>
+                 
+                 ) 
                     
                 
 
