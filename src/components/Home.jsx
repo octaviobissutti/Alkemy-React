c59167b7dbde701c2f231872f7b06be9e3ce8149
octaviobissutti <octavio_bissutti@hotmail.com>
2021-09-19 21:04:53 -0300

    Changes

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect} from 'react'
 import axios from 'axios';
 
 const Home = () => {
+    const [hero, setHero] = useState([]);
     const ACCESS_TOKEN= 10227175874088628;
     const BASE_URL= "https://superheroapi.com/api.php/";
     const URL = `${BASE_URL}${ACCESS_TOKEN}`;
@@ -9,10 +10,21 @@ const Home = () => {
 
     const apiCall = async () => {
     const api = await axios.get(`${BASE_URL}${ACCESS_TOKEN}/1`);
-    const response = api.data
-    return response.image;
+    const response = api.data;
+    if(response && response.data) {
+        const newHero = [...hero, ...response.data];
+        setHero(newHero);
     }
-    
+    // console.log(Object.values(response.powerstats.intelligence));
+    // console.log(response);
+    }
+    // const searchSuperHeros = async () => {
+    //     const response = await axios.get(`http://localhost:5000/${term}`);
+    //     if (response && response.data && response.data.results) {
+    //       const newHeros = [...heros, ...response.data.results].reverse();
+    //       setHeros(newHeros);
+    //     }
+    //   };
     useEffect(() => {
         apiCall();
     }, [])
@@ -20,9 +32,9 @@ const Home = () => {
     return (
         <div>
             {
-                <div>
+             hero.length > 0 && <span>{hero.name}</span>
                     
-                </div>
+                
 
             }
             
